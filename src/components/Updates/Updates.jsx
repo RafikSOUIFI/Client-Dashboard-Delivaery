80516e22a42c8016c00855e2c3dc651d8674f749
RafikSOUIFI <rafik.souifi@gmail.com>
2023-07-31 20:16:08 +0100

    change website

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -8,7 +8,7 @@ const Updates = () => {
           <div style={{ marginBottom: '0.5rem' }}>
             <span>Nous mettons à votre disposition un services client pour satisfaire au mieux vos besoins.<br/><br/>
              Pour toutes informations vous pouvez nous contacter sur :</span>
-              <span style={{ color: 'red' }}><br/>22 143 383</span>
+              <span style={{ color: 'red' }}><br/>58 023 439</span>
           </div>
           <span>Services client de lundi au samedi de <span style={{ color: 'red' }}>8h30</span> à <span style={{ color: 'red' }}>17h</span></span>
         </div>
